3a178c0dd165a909e9baecacd83a1a9c4716ea80
Valeria Shirko <valerka.shirko@gmail.com>
2020-10-25 01:14:33 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check (str, bracketsConfig) {
+  const stack = [];
+  const brackets = Object.fromEntries(bracketsConfig); 
+  let arr = str.split('')
+  for (let i of arr) {
+      if (stack[0] === i) {
+          stack.shift();
+      } else if (brackets[i]) {
+          stack.unshift(brackets[i]);
+      } else {
+          return false;
+      }
+  }
+  return stack.length === 0;
 }
